116d4509efdbabd95b7fbeae2d98877c9e643c16
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2025-03-24 10:17:16 -0300

    update

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
+
+export default function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className='container-center'>
+      <div className='login'>
+        <div className='login-area'>
+           <img src={logo} alt="Logo do sistema" />
+        </div>
+
+        <form>
+          <h1>Nova Conta</h1>
+          <input
+            type="text"
+            placeholder='Nome'
+            value={name}
+            onChange={ (e) => setName(e.target.value) }
+          />
+
+          <input
+            type="text"
+            placeholder='Email'
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+
+          <input
+            type="password"
+            placeholder='Senha'
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já possui uma conta?</Link>
+      </div>
+    </div>
+  )
+}
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwzjLY1E_EiA80tjko5ykI6gEb0_GAeuc",
+  authDomain: "tickets-cb613.firebaseapp.com",
+  projectId: "tickets-cb613",
+  storageBucket: "tickets-cb613.firebasestorage.app",
+  messagingSenderId: "322633789535",
+  appId: "1:322633789535:web:e2b9b1c763197b8d56abea",
+  measurementId: "G-JN6KNEYNVW"
+};
