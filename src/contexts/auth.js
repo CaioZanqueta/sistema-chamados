0fe7787ea8eff83f7e5e68183df3345c0702d639
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2025-03-25 21:47:50 -0300

    update

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,12 +1,40 @@
 import { useState, createContext, useEffect } from "react";
+import { auth, db } from '../services/firebaseConnection';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
+  const [loadingAuth, setLoadingAuth] = useState(false)
 
   function signIn(email, password) {
-    console.log(email, password)
+    console.log(email)
+    console.log(password)
+    alert('logado')
+  }
+
+  async function signUp(name, email, password) {
+    setLoadingAuth(true)
+
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then( async (value) => {
+      let uid = value.uid;
+
+      await setDoc(doc(db, "users", uid), {
+        name: name,
+        avatarUrl: null,
+      })
+      .then( () => {
+        alert("Cadastrado com sucesso")
+        setLoadingAuth(false)
+      } )
+    } )
+    .catch( (error) => {
+      console.log(error)
+      setLoadingAuth(false)
+    } )
   }
 
   return (
@@ -14,7 +42,8 @@ function AuthProvider({ children }) {
       value={{
         signed: !!user,
         user,
-        signIn
+        signIn,
+        signUp
       }}
     >
       {children}
